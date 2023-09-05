3a28dc5c1e8a690af6960f3071236cf826c81689
mitagmio <mitag.mio@gmail.com>
2023-09-05 08:49:08 +0000

    content

--- utils/content.ts
+++ utils/content.ts
@@ -1,10 +1,9 @@
-import { pseudoRandomBytes } from 'crypto';
-import { Address, Cell } from 'ton';
+import { beginCell, Cell, Dictionary } from 'ton-core';
+//import { Cell, beginCell, Address, beginDict, Slice, toNano } from "ton";
+import { Sha256 } from '@aws-crypto/sha256-js';
 
-export function randomAddress() {
-    return new Address(0, pseudoRandomBytes(256 / 8));
-}
-
+const ONCHAIN_CONTENT_PREFIX = 0x00;
+const SNAKE_PREFIX = 0x00;
 const OFF_CHAIN_CONTENT_PREFIX = 0x01;
 
 export function flattenSnakeCell(cell: Cell) {
@@ -13,17 +12,24 @@ export function flattenSnakeCell(cell: Cell) {
     let res = Buffer.alloc(0);
 
     while (c) {
-        let cs = c.beginParse();
-        let data = cs.readRemainingBytes();
+        const cs = c.beginParse();
+        if (cs.remainingBits === 0) {
+            return res;
+        }
+        if (cs.remainingBits % 8 !== 0) {
+            throw Error('Number remaining of bits is not multiply of 8');
+        }
+
+        const data = cs.loadBuffer(cs.remainingBits / 8);
         res = Buffer.concat([res, data]);
-        c = c.refs[0];
+        c = c.refs && c.refs[0];
     }
 
     return res;
 }
 
 function bufferToChunks(buff: Buffer, chunkSize: number) {
-    let chunks: Buffer[] = [];
+    const chunks: Buffer[] = [];
     while (buff.byteLength > 0) {
         chunks.push(buff.slice(0, chunkSize));
         buff = buff.slice(chunkSize);
@@ -31,43 +37,95 @@ function bufferToChunks(buff: Buffer, chunkSize: number) {
     return chunks;
 }
 
-export function makeSnakeCell(data: Buffer) {
-    let chunks = bufferToChunks(data, 127);
-    let rootCell = new Cell();
-    let curCell = rootCell;
+export function makeSnakeCell(data: Buffer): Cell {
+    const chunks = bufferToChunks(data, 127);
 
-    for (let i = 0; i < chunks.length; i++) {
-        let chunk = chunks[i];
+    if (chunks.length === 0) {
+        return beginCell().endCell();
+    }
 
-        curCell.bits.writeBuffer(chunk);
+    if (chunks.length === 1) {
+        return beginCell().storeBuffer(chunks[0]).endCell();
+    }
 
-        if (chunks[i + 1]) {
-            let nextCell = new Cell();
-            curCell.refs.push(nextCell);
+    let curCell = beginCell();
+
+    for (let i = chunks.length - 1; i >= 0; i--) {
+        const chunk = chunks[i];
+
+        curCell.storeBuffer(chunk);
+
+        if (i - 1 >= 0) {
+            const nextCell = beginCell();
+            nextCell.storeRef(curCell);
             curCell = nextCell;
         }
     }
 
-    return rootCell;
+    return curCell.endCell();
 }
 
 export function encodeOffChainContent(content: string) {
     let data = Buffer.from(content);
-    let offChainPrefix = Buffer.from([OFF_CHAIN_CONTENT_PREFIX]);
+    const offChainPrefix = Buffer.from([OFF_CHAIN_CONTENT_PREFIX]);
     data = Buffer.concat([offChainPrefix, data]);
     return makeSnakeCell(data);
 }
 
 export function decodeOffChainContent(content: Cell) {
-    let data = flattenSnakeCell(content);
+    const data = flattenSnakeCell(content);
 
-    let prefix = data[0];
+    const prefix = data[0];
     if (prefix !== OFF_CHAIN_CONTENT_PREFIX) {
         throw new Error(`Unknown content prefix: ${prefix.toString(16)}`);
     }
     return data.slice(1).toString();
 }
 
-export function unixNow() {
-    return Math.floor(Date.now() / 1000);
+export type ItemMetaDataKeys = 'name' | 'description';
+
+const itemOnChainMetadataSpec: {
+    [key in ItemMetaDataKeys]: 'utf8' | 'ascii' | undefined;
+} = {
+    name: 'utf8',
+    description: 'utf8',
+    // image: '',
+};
+
+const sha256 = (str: string) => {
+    const sha = new Sha256();
+    sha.update(str);
+    return Buffer.from(sha.digestSync());
+};
+
+export function buildOnChainMetadataCell(data: { [s: string]: string | undefined }): Cell {
+    const KEYLEN = 256;
+    const dict = Dictionary.empty(Dictionary.Keys.Buffer(KEYLEN), Dictionary.Values.Cell());
+
+    Object.entries(data).forEach(([k, v]: [string, string | undefined]) => {
+        if (!itemOnChainMetadataSpec[k as ItemMetaDataKeys]) throw new Error(`Unsupported onchain key: ${k}`);
+        if (v === undefined || v === '') return;
+
+        let bufferToStore = Buffer.from(v, itemOnChainMetadataSpec[k as ItemMetaDataKeys]);
+
+        const CELL_MAX_SIZE_BYTES = Math.floor((1023 - 8) / 8);
+
+        const rootCell = beginCell();
+        rootCell.storeUint(SNAKE_PREFIX, 8);
+        let currentCell = rootCell;
+
+        while (bufferToStore.length > 0) {
+            currentCell.storeBuffer(bufferToStore.subarray(0, CELL_MAX_SIZE_BYTES));
+            bufferToStore = bufferToStore.subarray(CELL_MAX_SIZE_BYTES);
+            if (bufferToStore.length > 0) {
+                const newCell = beginCell();
+                currentCell.storeRef(newCell);
+                currentCell = newCell;
+            }
+        }
+
+        dict.set(sha256(k), rootCell.asCell());
+    });
+
+    return beginCell().storeInt(ONCHAIN_CONTENT_PREFIX, 8).storeDict(dict).endCell();
 }
